a9f146be7e339282638a546e520e140365f3c2a0
Gary Sherman <gsherman@gmail.com>
2022-08-23 11:10:40 -0500

    Update import.bat

--- import.bat
+++ import.bat
@@ -13,7 +13,7 @@ REM c:\bin\diet.exe
 REM C:\bin\diet\2.2.0\mssql\diet.exe
 REM C:\bin\diet\2.2.1\mssql\diet.exe
 
-C:\bin\diet.exe -pick_one -license 655934BB -user_name sa -password sa -db_server . -db_name %1 -import %2 -objid -mto "last_close2case" -mto "reject_reason2case" %3 %4 %5 %6 
+C:\bin\diet.exe -pick_one -license SECRET -user_name sa -password sa -db_server . -db_name %1 -import %2 -objid -mto "last_close2case" -mto "reject_reason2case" %3 %4 %5 %6 
 REM -sqllog sql.log -log_file diet.log -mes_file diet.mes
 REM -parser -pparser -errors_max 200 -bad_fields 
 REM -lite
